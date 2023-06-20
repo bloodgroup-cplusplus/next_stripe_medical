d5ae88361ac1d4d49b4686c7dea7f11dfa8427db
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-06-20 07:59:32 +0530

    added a bunch of stuff

--- ProductCard.js
+++ ProductCard.js
@@ -1,7 +1,35 @@
+'use client'
 import React from 'react'
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 
 export default function ProductCard(props) {
+    const{product} = props
+    const {id:pric_id, unit_amount:cost,product:productInfo} = product
+    const {name,description} = productInfo
+
+    // link is important for seo 
+
+    function onProductClick()
+    {
+        Router.push()
+    }
+
+
+
+
+
+
   return (
-    <div>ProductCard</div>
+    <div className='flex flex-col shadow bg-white hover:shadow-lg cursor-pointer'>
+        <img className='w-full h-full object-cover' src={productInfo.images[0]} alt={name}/>
+        <div className='flex flex-col gap-2 p-4'>
+            <div className='flex items-center justify-between'>
+                <h3>{name}</h3>
+                <p>Rs {cost/100}</p>
+            </div>
+            <p className='text-sm'>{description}</p>
+        </div>
+    </div>
   )
 }
--- app/page.js
+++ app/page.js
@@ -19,8 +19,8 @@ export  default async function Home() {
   const products= await getStripeProducts()
   console.log(products)
   return (
-    <main className="p-4">
-      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3'>
+    <main className="p-4 flex flex-col">
+      <div className=' max-w-[1000px] w-full mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4'>
       {products.map((product,productIndex)=>{
 
         return (
