fa5c5f8ce61cd4b799eb1ea9ccfc84b253fd9609
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-06-20 06:49:46 +0530

    added icons

--- app/layout.js
+++ app/layout.js
@@ -12,12 +12,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
        
       </head>
       <body className= {'min-h-screen flex flex-col '+ inter.className}>
         
-        <header className='sticky top-0 p-6 bg-white border-b border-solid border-blue-900 shadow-md z-50 text-2xl sm:text-3xl md:text-4xl sm:p-8'>HEADER</header>
+        <header className='sticky top-0 p-6 bg-white border-b border-solid border-blue-900 shadow-md z-50 text-2xl sm:text-3xl md:text-4xl sm:p-8'>
+          HEADER
+          <i className='fa-solid fa-cart-shopping'></i>
+          </header>
+
         <div  className='flex-1'>
         {children} 
         </div>
