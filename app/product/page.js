463108de495fabc1683d5fb0c8a37e2692ce1724
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-06-20 08:23:05 +0530

    added loading back

--- app/product/page.js
+++ app/product/page.js
@@ -1,10 +1,16 @@
+'use client'
 import useCart from "../(store)/strore"
 export default function ProductPage(props)
 {
     const{searchParams} = props 
     const{price_id} = props
     const product= useCart(state=>state.product)
-    console.log(searchParams)
+    console.log(product)
+
+    if(!product?.name)
+    {
+        window.location.href='/'
+    }
 
         
     return (
