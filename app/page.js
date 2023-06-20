fc0adef16e66607c719cd093a4f6e6e2f15b2230
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-06-20 07:13:18 +0530

    added bunch of stuff

--- app/page.js
+++ app/page.js
@@ -1,8 +1,22 @@
 import Image from 'next/image'
+import Stripe from 'stripe'
+async function getStripeProducts()
+{
+  const stripe= new Stripe(process.env.STRIPE_SECRET ?? '',{
+    apiVersion:'2020-08-27'
+  })
+  const res = await stripe.prices.list({
+    expand:['data.product']
+  })
+  const prices = res.data;
+  return prices
+
+}
 
 export default function Home() {
+  const products=getStripeProducts()
   return (
-    <main className="padding-4">
+    <main className="p-4">
     </main>
   )
 }
