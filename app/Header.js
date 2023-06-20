024807641746b591c2ae6a80adec497629f58635
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-06-20 09:43:05 +0530

    added bunch of stuff

--- app/Header.js
+++ app/Header.js
@@ -1,15 +1,29 @@
-'use client'
+"use client"
+import Link from "next/link"
+import React from "react"
+import useCart from "./(store)/strore"
 
-import React from 'react'
-import Link from 'next/link'
+export default function Header()
+{
+    const cartItems= useCart(state=>state.cart)
+    return (
+        <header className="sticky top-0 p-6 bg-white border-b border-solid border-blue-900 shadow-md z-50 text-2xl sm:text-3xl md:text-4xl sm:p-8 flex item-center justify-between">
+            <Link href={'/'}>
+                <h1 className="uppercase cursor-pointer hover:scale-110">Suvidha</h1>
+            </Link>
+            <div className="relative grid place-items-center">
+                {cartItems.length &&(
+                    <div className="absolute aspect-square sm:h-5 grid place-items-center top-0 bg-blue-400 text-white rounded-full right-0 -translate-y-1/2 teanslate-x-1/2">
+                        <p className="sm:text-sm">{cartItems.length}</p>
+                    </div>
 
-export default function Header() {
-  return (
-    <header className='sticky top-0 p-6 bg-white border-b border-solid border-blue-900 shadow-md z-50 text-2xl sm:text-3xl md:text-4xl sm:p-8 flex item-center justify-between'>
-    <Link href={"/"}>
-    <h1 className='uppercase cursor-pointer hover:scale-110'>Suvidha</h1>
-    </Link>
-    <i className='fa-solid coursor-pointer hover:text-slate-500 fa-cart-shopping'></i>
-    </header>
-  )
+                )} 
+                <i className="fa-solid cursor-pointer hover:text-slate-500 fa-cart-shopping"></i>
+            </div>
+
+
+
+
+        </header>
+    )
 }
--- app/product/page.js
+++ app/product/page.js
@@ -40,7 +40,7 @@ export default function ProductPage(props)
                     <p className="md:text-base">Rs {cost/100}</p>
                     </div>
                     <p className="text-sm flex-1">{description}</p>
-                    <button className="bg-slate-700 text-white hover:bg-slate-500 cursor-pointer ml-auto px-4 py-2">Add to Cart</button>
+                    <button onClick={handleAddToCart} className="bg-slate-700 text-white hover:bg-slate-500 cursor-pointer ml-auto px-4 py-2">Add to Cart</button>
                 </div>
 
             </div>
