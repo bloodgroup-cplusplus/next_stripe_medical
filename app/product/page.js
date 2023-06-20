59d48cfb77cf00efa1cb329af38551b18f27416f
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-06-20 08:34:07 +0530

    added a bunch of stuff

--- app/product/page.js
+++ app/product/page.js
@@ -5,7 +5,10 @@ export default function ProductPage(props)
     const{searchParams} = props 
     const{price_id} = props
     const product= useCart(state=>state.product)
-    console.log(product)
+    const{cost,productInfo,name,description} = product
+    //console.log(product)
+    // console.log(searchParams)
+
 
     if(!product?.name)
     {
@@ -14,9 +17,21 @@ export default function ProductPage(props)
 
         
     return (
-        <div>
+        <div className="flex flex-col">
+            <div className="grid grid-cols-1 md:grid-cols-2 w-full max-w-[1000px] mx-auto">
+                <div className="md:p-2 shadow ">
+                    <img src={productInfo.images[0]} alt={name} className="w-full h-full object-cover"/>
+                </div>
+                <div className="flex flex-col gap-2 p-4">
+                    <div className="flex md:flex-col md:items-start  text-xl sm:items-items-center justify-between gap-2">
+                    <h3>{name}</h3>
+                    <p className="md:text-base">Rs {cost/100}</p>
+                    </div>
+                    <p className="text-sm">{description}</p>
+                    <button className="bg-slate-700 text-white hover:bg-slate-500 cursor-pointer ml-auto px-4 py-2">Add to Cart</button>
+                </div>
 
-
+            </div>
         </div>
     )
 }
